771bc727ac32940ce6c004ca068837253705bfe2
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2023-03-20 21:32:56 -0400

    v1.0.0

--- reset-moodle-inputs/reset.js
+++ reset-moodle-inputs/reset.js
@@ -2,12 +2,15 @@
 
 for (const option of document.querySelectorAll("option")) {
   option.selected = false;
+  option.disabled = false;
+  option.closest("select").disabled = false;
 }
 
 for (const input of document.querySelectorAll(
   'input[type="radio"], input[type="checkbox"]'
 )) {
   input.checked = false;
+  input.disabled = false;
 }
 
 for (const icon of document.querySelectorAll(".text-danger, .text-success")) {
