c01a82168b52dee4897de2f9a78f297c9eca4615
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-03-14 15:48:40 -0400

    reduce image quality to 75%

--- src/utils/image-to-base64.ts
+++ src/utils/image-to-base64.ts
@@ -1,9 +1,10 @@
 /**
  * Convert an image html element into a base64 image string
  * @param imageElement
+ * @param quality (default: 0.75 -> 75%)
  * @returns
  */
-function imageToBase64(imageElement: HTMLImageElement): Promise<string | null> {
+function imageToBase64(imageElement: HTMLImageElement, quality = 0.75): Promise<string | null> {
   return new Promise(resolve => {
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
@@ -21,7 +22,7 @@ function imageToBase64(imageElement: HTMLImageElement): Promise<string | null> {
       canvas.height = img.height;
       ctx.drawImage(img, 0, 0);
 
-      const base64 = canvas.toDataURL('image/png');
+      const base64 = canvas.toDataURL('image/png', quality);
       resolve(base64);
 
       canvas.remove();
