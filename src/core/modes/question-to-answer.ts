2f06370bb787a3ea1e45c189349bdd5ffefd4a0b
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-03-20 17:17:12 -0400

    fixed question to answer pre-wrap on click

--- src/core/modes/question-to-answer.ts
+++ src/core/modes/question-to-answer.ts
@@ -17,19 +17,15 @@ function questionToAnswerMode(props: Props) {
   props.removeListener();
 
   const questionBackup = questionElement.textContent;
-
   questionElement.textContent = props.gptAnswer.response;
-
-  // Format the content
   questionElement.style.whiteSpace = 'pre-wrap';
 
   // To go back to the question / answer
-  let contentIsResponse = true;
   questionElement.addEventListener('click', function () {
-    questionElement.style.whiteSpace = contentIsResponse ? '' : 'pre-warp';
-    questionElement.textContent = contentIsResponse ? questionBackup : props.gptAnswer.response;
+    const contentIsResponse = questionElement.textContent === props.gptAnswer.response;
 
-    contentIsResponse = !contentIsResponse;
+    questionElement.style.whiteSpace = contentIsResponse ? 'initial' : 'pre-wrap';
+    questionElement.textContent = contentIsResponse ? questionBackup : props.gptAnswer.response;
   });
 }
 
